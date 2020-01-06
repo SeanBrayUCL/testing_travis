8c1fe2f300f14e147303c93a809625e408a54cfe
Sean_Bray_UCL <ucapsb5.ucl.ac.uk>
2020-01-06 20:39:32 +0000

    test

--- travelplanner.py
+++ travelplanner.py
@@ -110,37 +110,6 @@ plot_map(route)
 plot_bus_load(route, passengers)
 
 
-def route_cc(route):
-    '''
-    Converts a set of route into a Freeman chain code
-    3 2 1
-    \ | /
-   4 - C - 0
-    / | \
-    5 6 7 
-    '''
-    start = route[0][:2]
-    cc = []
-    freeman_cc2coord = {0: (1, 0),
-                        1: (1, -1),
-                        2: (0, -1),
-                        3: (-1, -1), 
-                        4: (-1, 0),
-                        5: (-1, 1),
-                        6: (0, 1),
-                        7: (1, 1)}
-    freeman_coord2cc = {val: key for key,val in freeman_cc2coord.items()}
-    for b, a in zip(route[1:], route):
-        x_step = b[0] - a[0]
-        y_step = b[1] - a[1]
-        cc.append(str(freeman_coord2cc[(x_step, y_step)]))
-    return start, ''.join(cc)
-
-start_point, cc = route_cc(route)
-print((f"The bus route starts at {start_point} and\n"
-f"it's described by this chain code:\n{cc}"))
-
-
 
 
 
